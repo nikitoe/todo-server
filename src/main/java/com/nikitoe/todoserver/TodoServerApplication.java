9159853a7566ac89ba7ab9f199074e8e05148972
Jiyong Sim <sjy19910222@gmail.com>
2023-03-05 04:02:56 +0900

    feat : todo list controller 추가

--- src/main/java/com/nikitoe/todoserver/TodoServerApplication.java
+++ src/main/java/com/nikitoe/todoserver/TodoServerApplication.java
@@ -3,9 +3,10 @@ package com.nikitoe.todoserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class TodoServerApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        SpringApplication.run(TodoServerApplication.class, args);
     }
 
 }
