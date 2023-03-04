a0afbe241b6dfa877d506491f702972a30347fe2
Jiyong Sim <sjy19910222@gmail.com>
2023-03-04 17:18:23 +0900

    init : 초기 설정

---
+++ src/main/java/com/nikitoe/todoserver/TodoServerApplication.java
@@ -0,0 +1,11 @@
+package com.nikitoe.todoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class TodoServerApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
