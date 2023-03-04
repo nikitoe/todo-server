4a80df6fe288d287609bc49caa306d8c6372fcc9
Jiyong Sim <sjy19910222@gmail.com>
2023-03-04 19:52:20 +0900

    feat : todo list repository 추가

---
+++ src/main/java/com/nikitoe/todoserver/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.nikitoe.todoserver.repository;
+
+
+import com.nikitoe.todoserver.model.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+
+}
